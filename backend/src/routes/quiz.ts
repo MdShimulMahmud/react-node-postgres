ecc9d2fa22ab2fbaec3b5fd050381d5901fbc897
MdShimulMahmud <shimulmahmud18@gmail.com>
2024-10-02 23:47:37 +0600

    updated user authentication and authorization

--- backend/src/routes/quiz.ts
+++ backend/src/routes/quiz.ts
@@ -1,19 +1,25 @@
 import express from "express";
 import { QuestionController } from "../controllers/QuestionController";
 import { QuizController } from "../controllers/QuizController";
+import { authMiddleware } from "../middlewares/authMiddleware";
 
 const router = express.Router();
 const quizController = new QuizController();
 const questionController = new QuestionController();
 
 // Quiz routes
-router.post("/quizzes", quizController.createQuiz.bind(quizController));
+router.post(
+  "/quizzes",
+  authMiddleware,
+  quizController.createQuiz.bind(quizController)
+);
 router.get("/quizzes", quizController.getAllQuizzes.bind(quizController));
 router.get("/quizzes/:id", quizController.getQuiz.bind(quizController));
 
 // Question routes
 router.post(
   "/quizzes/:quizId/questions",
+  authMiddleware,
   questionController.createQuestion.bind(questionController)
 );
 
